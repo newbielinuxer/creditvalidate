9fbe89afbdd5e32194bdffe7a30ea0e2ace1f1b7
newbielinuxer <83208697+newbielinuxer@users.noreply.github.com>
2024-05-25 09:45:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 // index.js
 
+// Luhn Algorithm implementation
+function isValidCardNumber(number) {
+  // Convert the number to a string and remove any non-digit characters
+  const sanitized = number.toString().replace(/\D/g, '');
+  
+  let sum = 0;
+  let shouldDouble = false;
+  
+  // Iterate over the digits in reverse order
+  for (let i = sanitized.length - 1; i >= 0; i--) {
+    let digit = parseInt(sanitized.charAt(i));
+    
+    if (shouldDouble) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    
+    sum += digit;
+    shouldDouble = !shouldDouble;
+  }
+  
+  return (sum % 10) === 0;
+}
+
+// Export the function
+module.exports = { isValidCardNumber };
+
 const cardTypes = {
   visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
   mastercard: /^5[1-5][0-9]{14}$/,
